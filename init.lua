edb1aad3256e452e714df4da80007fc3a0fc1e12
Okadesde <okadesde@googlemail.com>
2015-04-12 15:16:27 +0200

    initial commit
    working example

---
+++ init.lua
@@ -0,0 +1,18 @@
+--- config
+ssid = ""                   -- WiFi Name 
+password = ""     			-- WiFi Password
+---
+
+print("Setting up WIFI...")
+wifi.setmode(wifi.STATION)
+wifi.sta.config(ssid,password)
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+if wifi.sta.getip()== nil then 
+print("IP unavaiable, Waiting...") 
+else 
+tmr.stop(1)
+print("Config done, IP is "..wifi.sta.getip())
+dofile("main.lua")
+end 
+end)
---
+++ main.lua
@@ -0,0 +1,65 @@
+--- config
+tmr.delay(1000000)
+hum="XX"
+tem="XX"
+PIN = 4                             -- GPIO2
+contentLength = 0                   
+base64login = ""    				-- user:pwassword (pimatic) in BASE64; ex. "dXNlcjpwYXNzd29yZA=="
+pimaticServer = ""    				-- pimatic server IP
+interval = 300                      -- send data every X seconds
+
+--- get temp
+function readDHT22()
+    dht22 = require("dht22")
+    dht22.read(PIN)
+    t = dht22.getTemperature()
+    h = dht22.getHumidity()
+    hum=(h/10).."."..(h%10)
+    tem=(t/10).."."..(t%10)
+    print("Humidity:    "..hum.."%")
+    print("Temperature: "..tem.." deg C")
+    -- release module
+    dht22 = nil
+    package.loaded["dht22"]=nil
+end
+
+--- calc content-length
+function calcLength(type)
+    print(type)
+    contentLength = string.len(type) + 40
+    print("Content-Length: "..contentLength)
+    
+end
+
+--- send data
+function sendData(type, name)
+    calcLength(type)
+    print("Sending data ...")
+    conn=net.createConnection(net.TCP, 0) 
+    conn:on("receive", function(conn, payload) print(payload) end)
+    conn:connect(80,pimaticServer)
+    conn:send("PATCH /api/variables/esp01-"..name.." HTTP/1.1\r\n")
+    conn:send("Authorization: Basic "..base64login.."\r\n")
+    conn:send("Host: "..pimaticServer.."\r\n")
+    conn:send("Content-Type:application/json\r\n")
+    conn:send("Content-Length: "..contentLength.."\r\n\r\n")
+    conn:send("\{\"type\"\: \"value\"\, \"valueOrExpression\"\: "..type.."\}")
+    ---
+    conn:on("sent",function(conn)
+        print("Closing connection")
+        conn:close()
+    end)
+    conn:on("disconnection", function(conn)
+        print("Got disconnection...")
+    end)
+    ---
+    
+end
+
+--- main loop 
+tmr.alarm(0, (interval*1000), 1, function() 
+    readDHT22()
+    sendData(hum, "hum")
+    tmr.delay(1000000)
+    sendData(tem, "tem")
+end )
